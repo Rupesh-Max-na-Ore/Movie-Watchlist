283240dd169d6e58c5a435792a6644c1ef3a39e8
Rupesh <dasrupesh2124@gmail.com>
2025-07-10 17:38:58 +0530

    database few major functions construction completed

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-# title, release_date, watched
+import datetime
+import sqlite3
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
@@ -12,3 +13,37 @@ INSERT_MOVIES = (
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestmp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM MOVIES WHERE watched = 1;"
+
+connection = sqlite3.connect("data.db")
+
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
+
+
+def add_movie(title, release_timestamp):
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+
+def get_movies(upcoming=False):
+    with connection:
+        cursor = connection.cursor()
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+
+def watch_movie(title):
+    pass
+
+
+def get_watched_movie():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
